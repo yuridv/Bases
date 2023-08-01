bc8e0fc447019499ecc14b3348e1a5504a34ac32
Yuri Eduardo <57859317+yuriedu@users.noreply.github.com>
2023-08-01 20:37:04 -0300

    UPDATE

--- FrontEnd/html/public/js/index.js
+++ FrontEnd/html/public/js/index.js
@@ -1,23 +1,28 @@
 (async () => {
-  let url = false;
-  var parms = window.location.pathname.split('/')
-  if (parms[3] && !url) url = `/views/${parms[1]}/${parms[2]}/${parms[3]}.html`;
-  if (parms[2] && !url) url = `/views/${parms[1]}/${parms[2]}.html`;
-  if (parms[1] && !url) url = `/views/${parms[1]}.html`;
+  let url = '/views'+window.location.pathname+'.html';
   if (url == '/views/main.html') return window.location.href = '/';
-  if (!url) url = '/views/main.html'
+  if (url == '/views/.html') url = '/views/main.html'
 
-  let html = await fetch(url || `/views/main.html`)
+  let html = await fetch(url.toLowerCase())
     .then(response => response.text())
     .catch(() => {})
   if (!html) return window.location.href = '/';
 
-  let index = '<!DOCTYPE html>\n<html lang="en">'+document.documentElement.innerHTML+'</html>'
-  let body = index.slice(index.indexOf('<body>') + 6, index.indexOf('</body>'));
+  document.body.innerHTML = html
 
-  document.open();
-  document.write(index.replace(body, html));
-  document.close();
+  let scripts = document.body.getElementsByTagName('script')
+
+  // let script = document.createElement("script");
+  // script.innerHTML = scripts[0].innerHTML;
+  // document.body.appendChild(script);
+
+
+  // let index = '<!DOCTYPE html>\n<html lang="en">'+document.documentElement.innerHTML+'</html>'
+  // let body = index.slice(index.indexOf('<body>') + 6, index.indexOf('</body>'));
+
+  // document.open();
+  // document.write(index.replace(body, html));
+  // document.close();
 
   let css = url ? url.replace('/views/','/public/css/').replace('.html','.css') : false;
   if (css) document.getElementById('css').href = css || `/public/css/main.css`;
